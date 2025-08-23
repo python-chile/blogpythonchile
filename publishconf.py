ff949e0ccc78dd7bd4aaae7de4d55aa614f71384
Tony-Rome <tonyn.rome@gmail.com>
2025-08-23 19:28:53 -0400

    feat: base siteurl

--- publishconf.py
+++ publishconf.py
@@ -9,7 +9,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 #SITEURL = "https://blog.pythonchile.cl"
-SITEURL = ""
+SITEURL = "https://python-chile.github.io/blogpythonchile/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
