ad6fcdc4e82d2fbc9e9906dfcbbd61122465ce8a
Tony-Rome <tonyn.rome@gmail.com>
2025-08-23 18:58:42 -0400

    feat: deploy gp config

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 #SITEURL = "https://blog.pythonchile.cl"
+SITEURL = "https://python-chile.github.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
