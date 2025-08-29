2ec462ec9873a8e06d31791c7bf21ea4e53384b7
Tony-Rome <tonyn.rome@gmail.com>
2025-08-28 21:54:53 -0400

    feat: cname url

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-#SITEURL = "https://blog.pythonchile.cl"
-SITEURL = "https://python-chile.github.io/blogpythonchile/"
+SITEURL = "https://blog.pythonchile.cl"
+#SITEURL = "https://python-chile.github.io/blogpythonchile/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
